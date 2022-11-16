7b52808be3919c91f0865bb5aacf4e77d4eea04a
Dennis van der Wals <dennis@vdwals.de>
2022-11-16 16:46:31 +0100

    Ignore empty rows

--- src/fyayc/mu/dvw/trc/App.java
+++ src/fyayc/mu/dvw/trc/App.java
@@ -6,8 +6,11 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.SecureRandom;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class App {
 
@@ -32,11 +35,11 @@ public class App {
 
     List<String> candidates = Collections.emptyList();
     try {
-      candidates = Files.readAllLines(path, StandardCharsets.UTF_8);
+      candidates = new ArrayList<>(Files.readAllLines(path, StandardCharsets.UTF_8).stream()
+          .filter(s -> s.length() > 0).collect(Collectors.toSet()));
     } catch (IOException e) {
       e.printStackTrace();
     }
-
 
     if (candidates.isEmpty()) {
       System.out.println("No candidates in " + path.toAbsolutePath());
@@ -53,10 +56,28 @@ public class App {
     System.out.println();
     System.out.println("Chosen:");
     for (int index = 0; index < chose; index++) {
+      try {
+        TimeUnit.SECONDS.sleep(5);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
       System.out.println(" * " + candidates.get(index));
+      // printName(candidates.get(index));
     }
 
+  }
 
+  private static void printName(String name) {
+    System.out.print(" * ");
+    for (int i = 0; i < name.length(); i++) {
+      try {
+        TimeUnit.SECONDS.sleep(2);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      System.out.print(name.charAt(i));
+    }
+    System.out.println();
   }
 
 }
